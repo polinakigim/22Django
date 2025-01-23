add11a644f4f351cb12e30bf31e1257f124c1f31
polinakigim <polinakigim@gmail.com>
2025-01-23 11:32:47 +0300

    task 2 create models Category and Product; change poetry.lock

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length="100",
+        verbose_name="Наименование",
+        help_text="Введите наименование продукта",
+    )
+    description = models.TextField(
+        blank=True, null=True, verbose_name="Описание продукта"
+    )
+    photo = models.ImageField(
+        upload_to="product/photo",
+        blank=True,
+        null=True,
+        verbose_name="Фото",
+        help_text="Загрузите фото продукта",
+    )
+    category = models.ForeignKey(
+        "Category",
+        max_length="100",
+        verbose_name="Категория",
+        help_text="Введите категорию",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="products",
+    )
+    price = models.IntegerField(
+        verbose_name="Цена за покупку", help_text="Введите цену за штуку товара"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(
+        auto_now_=True, verbose_name="Дата последнего изменения"
+    )
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name", "category"]
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length="100",
+        verbose_name="Наименование",
+        help_text="Введите наименование категории",
+    )
+    description = models.TextField(
+        blank=True, null=True, verbose_name="Описание категории"
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
